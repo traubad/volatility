35a3828cac77cc62187473e910f0bf1edc0c22f8
Adam Traub <traubad@vcu.edu>
2020-11-01 15:14:36 -0500

    user no longer needs to enter the annoying 'yes' text in order to run the test

--- tests/simpleTest.py
+++ tests/simpleTest.py
@@ -3,15 +3,15 @@ import test_settings
 
 def run_test():
     runs = (
-        "python2 {} --profile={} -f {} escalate -n cmd.exe"
+        "python2 {} --profile={} -f {} escalate -n explorer.exe --write"
             .format(test_settings.vol_path, test_settings.profile, test_settings.mem),
-        "python2 {} --profile={} -f {} escalate -i 1604"
+        "python2 {} --profile={} -f {} escalate -i 1812 --write"
             .format(test_settings.vol_path, test_settings.profile, test_settings.mem),
     )
     try:
         for run in runs:
-            os.system(run)
-            
+            os.system('echo "Yes, I want to enable write support" | '+run)
+
     except Exception:
         print("Failure")
 
